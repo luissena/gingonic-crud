efcd327d763ae4ffd2b82fc403b46995d2a5b3f7
Luis Sena <luis.senarode@gmail.com>
2023-09-19 16:36:02 -0300

    feat: handler request

---
+++ handler/request.go
@@ -0,0 +1,31 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param %s is required and must be %s", name, typ)
+}
+
+type CreatePostRequest struct {
+	Author string `json:"author"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
+func (r *CreatePostRequest) Validate() error {
+
+	if r.Author == "" && r.Title == "" && r.Body == "" {
+		return fmt.Errorf("request body is empty or malformed")
+	}
+
+	if r.Author == "" {
+		return errParamIsRequired("author", "string")
+	}
+	if r.Title == "" {
+		return errParamIsRequired("title", "string")
+	}
+	if r.Body == "" {
+		return errParamIsRequired("body", "string")
+	}
+	return nil
+}
